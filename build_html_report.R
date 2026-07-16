b9195b0f1629ae16ccc187f02825dcfcbf76b99a
CamiloAlfonso <kmialfonso@gmail.com>
2026-07-15 23:43:12 -0500

    Organize project and Github upload 2

--- build_html_report.R
+++ build_html_report.R
@@ -3,6 +3,6 @@ library(rmarkdown)
 render(
   input = "Scripts/NestboxOrientationTRES.Rmd",
   output_file = "NestboxOrientationTRES.html",
-  output_dir = "Output",
+  output_dir = "docs",
   clean = TRUE
 )
